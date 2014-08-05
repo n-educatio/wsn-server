ca39d1b7fbf43745b4808067db877e8388d4e245
Kuba Biernacki <kbiernacki@n-educatio.pl>
2014-08-05 13:29:41 +0000

    logger can write to given stream

--- src/Neducatio/WebSocketNotification/Common/Logger.php
+++ src/Neducatio/WebSocketNotification/Common/Logger.php
@@ -2,12 +2,29 @@
 namespace Neducatio\WebSocketNotification\Common;
 
 /**
- * Simple logger that prints everything to STDOUT
+ * Simple logger that prints everything to given stream (STDOUT by default)
  */
 class Logger implements Loggable
 {
+  private $stream;
+
+  public function __construct($stream = STDOUT)
+  {
+    if (!is_resource($stream) || 'stream' !== get_resource_type($stream)) {
+      throw new \InvalidArgumentException('You should provide resource of type stream');
+    }
+    
+    $this->stream = $stream;
+  }
+
+  /**
+   * Prints everything to given stream
+   *
+   * @param string $level
+   * @param string $message
+   */
   public function log($level, $message)
   {
-    fprintf(STDOUT, "[%s][%s]\t%s\n", date('Y-d-m H:i:s'), $level, $message);
+    fprintf($this->stream, "[%s][%s]\t%s\n", date('Y-d-m H:i:s'), $level, $message);
   }
 }
