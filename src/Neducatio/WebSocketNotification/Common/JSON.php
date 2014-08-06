a7415f6154fe703f338c33552c47778e3cb9a1ce
Kuba Biernacki <kbiernacki@n-educatio.pl>
2014-08-06 08:53:49 +0000

    php 5.4 compability

--- src/Neducatio/WebSocketNotification/Common/JSON.php
+++ src/Neducatio/WebSocketNotification/Common/JSON.php
@@ -40,7 +40,29 @@ class JSON
   protected static function checkErrors()
   {
     if (JSON_ERROR_NONE !== ($errorCode = json_last_error())) {
-        throw new \RuntimeException(json_last_error_msg(), $errorCode);
+      throw new \RuntimeException(self::getErrorMessage($errorCode), $errorCode);
     }
+  }
+
+  private static function getErrorMessage($errorCode)
+  {
+      if (!function_exists('json_last_error_msg')) {
+        switch ($errorCode) {
+            case JSON_ERROR_DEPTH:
+                return 'Maximum stack depth exceeded';
+            case JSON_ERROR_STATE_MISMATCH:
+                return 'Underflow or the modes mismatch';
+            case JSON_ERROR_CTRL_CHAR:
+                return 'Unexpected control character found';
+            case JSON_ERROR_SYNTAX:
+                return 'Syntax error, malformed JSON';
+            case JSON_ERROR_UTF8:
+                return 'Malformed UTF-8 characters, possibly incorrectly encoded';
+            default:
+                return '';
+        }
+      } else {
+        return json_last_error_msg();
+      }
   }
 }
